0afb3a7ed36946352f16dab1b8b9aca0b0625e73
Piyush Poddar <piyushpoddar@Aarushis-MacBook-Air.local>
2014-10-31 22:18:10 -0400

    Cast contact_id

--- server/matches.js
+++ server/matches.js
@@ -1,7 +1,7 @@
 var PG = require('./knex');
 
 exports.getMatches = function(req, res) {
-	var contact_id = req.query.contact_id;
+	var contact_id = Number(req.query.contact_id);
 
 	PG.knex.raw("SELECT c1.contact_id first_contact_id, c1.guessed_full_name first_contact_name, c1.guessed_gender first_contact_gender, c2.contact_id second_contact_id, c2.guessed_full_name second_contact_name, c2.guessed_gender second_contact_gender, ? matcher_contact_id \
 	FROM contacts c1, contacts c2  \
