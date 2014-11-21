bd8e05ed37ee85ccd2816dd01a3efb51facc699c
Piyush Poddar <piyushpoddar@Aarushis-MacBook-Air.local>
2014-11-20 23:51:44 -0500

    logging

--- server/matches.js
+++ server/matches.js
@@ -125,6 +125,7 @@ exports.addMatchResult = function(req, res) {
 					//Insert new match
 					PG.knex('pairs').insert({first_contact_id: firstRecipient.contact_id, second_contact_id: secondRecipient.contact_id, matcher_contact_id: matcher_contact_id, is_anonymous: is_anonymous, first_contact_status:"NOTIFIED"},'pair_id').then(function(result) {
 						var pair_id = result[0].pair_id;
+						console.log("Successfully inserted match with pair_id: ", result);
 						var matchURL = "matchflare.com/" + int_encoder.encode(pair_id);
 						messageToText = messageToText + " See " + recipientGenderPronoun + " and learn more at " + matchURL + ". Text SAD to stop new matches";
 						sendTextMessage(firstRecipient.normalized_phone_number,messageToText);
